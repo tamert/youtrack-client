b2b316d5a1462dabd25269c17bfb401328624524
Nepomuk Fraedrich <info@nepda.eu>
2015-01-15 08:31:50 +0100

    Added example for executing a simple command

---
+++ examples/execute-command.php
@@ -0,0 +1,25 @@
+<?php
+
+include_once 'config.php';
+
+$youtrack = new YouTrack\Connection(
+    YOUTRACK_URL,
+    YOUTRACK_USERNAME,
+    YOUTRACK_PASSWORD
+);
+
+
+$issueId = 'Sandbox-25';
+$command = 'Assignee me';
+
+try {
+    $result = $youtrack->executeCommand($issueId, $command);
+    if ($result) {
+        echo "Command '$command' executed on issue '$issueId'." . PHP_EOL;
+    } else {
+        echo "The command was not executed" . PHP_EOL;
+    }
+} catch (\YouTrack\NotAuthorizedException $e) {
+    echo $e->getMessage() . PHP_EOL;
+}
+
