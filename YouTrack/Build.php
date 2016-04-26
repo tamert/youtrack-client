60f903f576372ed5ab58eaf77f940db86d27346b
Nepomuk Fraedrich <info@nepda.eu>
2016-04-26 14:42:45 +0200

    Cleanup Build class

--- YouTrack/Build.php
+++ YouTrack/Build.php
@@ -3,13 +3,7 @@ namespace YouTrack;
 
 /**
  * A class describing a youtrack build.
- *
- * @todo add properties and methods docs
  */
 class Build extends Object
 {
-    public function __construct(\SimpleXMLElement $xml = null, Connection $youtrack = null)
-    {
-        parent::__construct($xml, $youtrack);
-    }
 }
