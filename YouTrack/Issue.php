394831068a5c8b7b83da53cbbc1623d85531b4d8
gkertai <gabor@production-minds.com>
2017-01-11 19:30:31 +0100

    Add support for Issue::state and Issue::estimation

--- YouTrack/Issue.php
+++ YouTrack/Issue.php
@@ -7,44 +7,66 @@ namespace YouTrack;
  * @property string id Issue id in database
  * @method string getId() Issue id in database
  * @method setId(string $id) Issue id in database
+ *
  * @property string|null jiraId If issue was imported from JIRA, represents id, that it have in JIRA
  * @method string|null getJiraId() If issue was imported from JIRA, represents id, that it have in JIRA
  * @method setJiraId(string $jiraId) If issue was imported from JIRA, represents id, that it have in JIRA
+ *
  * @property string projectShortName Short name of the issue's project
  * @method string getProjectShortName() Short name of the issue's project
  * @method setProjectShortName(string $projectShortName) Short name of the issue's project
+ *
  * @property int numberInProject Number of issue in project
  * @method int getNumberInProject() Number of issue in project
  * @method setNumberInProject(int $numberInProject) Number of issue in project
+ *
  * @property string|null summary Summary of the issue
  * @method string|null getSummary() Summary of the issue
  * @method setSummary(string $summary) Summary of the issue
+ *
  * @property string|null description Description of the issue
  * @method string|null getDescription() Description of the issue
  * @method setDescription(string $description) Description of the issue
+ *
  * @property int created Time when issue was created (the number of milliseconds since January 1, 1970, 00:00:00 GMT represented by this date).
  * @method int getCreated() Time when issue was created (the number of milliseconds since January 1, 1970, 00:00:00 GMT represented by this date).
+ *
  * @property int updated Time when issue was last updated (the number of milliseconds since January 1, 1970, 00:00:00 GMT represented by this date).
  * @method int getUpdated() Time when issue was last updated (the number of milliseconds since January 1, 1970, 00:00:00 GMT represented by this date).
+ *
  * @property string updaterName Login of the user, that was the last, who updated the issue
  * @method string getUpdaterName() Login of the user, that was the last, who updated the issue
  * @method setUpdaterName(string $updaterName) Login of the user, that was the last, who updated the issue
+ *
+ * @property string state
+ * @method string getState()
+ * @method setState(string $state)
+ *
  * @property int|null resolved If the issue is resolved, shows time, when resolved state was last set to the issue (the number of milliseconds since January 1, 1970, 00:00:00 GMT represented by this date).
  * @method int|null getResolved() If the issue is resolved, shows time, when resolved state was last set to the issue (the number of milliseconds since January 1, 1970, 00:00:00 GMT represented by this date).
  * @method setResolved(int $resolved) If the issue is resolved, shows time, when resolved state was last set to the issue (the number of milliseconds since January 1, 1970, 00:00:00 GMT represented by this date).
+ *
  * @property string reporterName Login of user, who created the issue
  * @method string getReporterName() Login of user, who created the issue
  * @method setReporterName(string $reporterName) Login of user, who created the issue
+ *
  * @property string voterName Login of user, that voted for issue
  * @method string getVoterName() Login of user, that voted for issue
  * @method setVoterName(string $voterName) Login of user, that voted for issue
+ *
  * @property int commentsCount Number of comments in issue
  * @method int getCommentsCount() Number of comments in issue
+ *
  * @property int votes Number of votes for issue
  * @method int getVotes() Number of votes for issue
+ *
  * @property string|null permittedGroup User group, that has permission to read this issue; if group is not set, it means that any user has access to this issue
  * @method string|null getPermittedGroup() User group, that has permission to read this issue; if group is not set, it means that any user has access to this issue
  * @method setPermittedGroup(string $permittedGroup) User group, that has permission to read this issue; if group is not set, it means that any user has access to this issue
+ *
+ * @property int estimation Time estimation in minutes
+ * @method int getEstimation()
+ * @method setEstimation(int $numberInProject)
  *
  * @link https://confluence.jetbrains.com/display/YTD65/Get+an+Issue
  */
@@ -81,6 +103,9 @@ class Issue extends BaseObject
                     }
                 }
                 $this->__set('attachments', $attachments);
+            }
+            if (isset($this->attributes['Estimation'])) {
+                $this->__set('estimation', (int)$this->attributes['Estimation']);
             }
         }
     }
